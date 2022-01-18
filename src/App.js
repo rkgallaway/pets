e93d24f7a476f4a9b2f31f9f9120c7770213d75e
Ryan Gallaway <rkgallaway@msn.com>
2022-01-18 12:19:05 -0800

    demo complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      species: '',
+      petData: [],
+      showPets: false
+    }
+  }
+
+  handleInput = e => this.setState({ species: e.target.value});
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    // console.log('button clicked');
+    // route  to hit:  http://localhost:3002/pets?species=dog
+    let url = `${process.env.REACT_APP_SERVER_URL}/pets?species=${this.state.species}`
+    let petResults = await axios.get(url)
+    console.log(petResults.data);
+    this.setState({
+      showPets: true,
+      petData: petResults.data
+    })
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <>
+        <h1>Pets</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>enter a pet species(dog, cat, fish)
+            <input type="text" onInput={this.handleInput} />
+          </label>
+          <button>ClickMe</button>
+        </form>
+
+        {
+          this.state.showPets &&
+          this.state.petData.map(pet => <h3>{pet.name}</h3>)
+        }
+      </>
+    );
+  }
 }
 
 export default App;
