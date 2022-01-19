8d939ab3852d836413d1aba9e992748a26c88561
Ryan Gallaway <rkgallaway@msn.com>
2022-01-19 11:51:10 -0800

    demo complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import axios from 'axios';
+import { Col, Row, Card } from 'react-bootstrap';
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       species: '',
       petData: [],
-      showPets: false
+      showPets: false,
+      showImages: false,
+      images: []
     }
   }
 
-  handleInput = e => this.setState({ species: e.target.value});
+  handleInput = e => this.setState({ species: e.target.value });
 
   handleSubmit = async e => {
     e.preventDefault();
@@ -28,8 +31,41 @@ class App extends React.Component {
     })
   }
 
+  handleImageType = e => this.setState({ imageType: e.target.value });
+
+  getImageInfo = async e => {
+    e.preventDefault();
+
+    let url = `${process.env.REACT_APP_SERVER_URL}/images?imageType=${this.state.imageType}`
+    let pictureResults = await axios.get(url)
+
+    console.log(pictureResults.data)
+    this.setState({
+      images: pictureResults.data,
+      showImages: true
+    })
+  }
+
   render() {
     console.log(this.state);
+    // this.src = pic.urls.regular;
+    // this.alt = pic.alt_description;
+    // this.description = pic.description;
+    // this.photographer = pic.user.name;
+
+
+    let imageCards = this.state.images.map((pic, idx) => (
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={pic.src} alt={pic.alt} />
+          <Card.Body>
+            <Card.Title>Photo By: {pic.photographer}</Card.Title>
+            <Card.Text>{pic.description}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    ))
+
     return (
       <>
         <h1>Pets</h1>
@@ -45,6 +81,18 @@ class App extends React.Component {
           this.state.showPets &&
           this.state.petData.map(pet => <h3>{pet.name}</h3>)
         }
+
+        <form onSubmit={this.getImageInfo}>
+          <label>Show Images
+            <input type="text" onInput={this.handleImageType} />
+          </label>
+          <button>Show. Me. The. Pics!</button>
+        </form>
+
+        <Row xs={1} sm={2} md={3} lg={4} >
+          {imageCards}
+        </Row>
+
       </>
     );
   }
